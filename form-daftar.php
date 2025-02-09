e762807de2cef1d95b52b5261a726b2a0fa4d517
birugh <notblue.https@gmail.com>
2025-02-09 17:02:47 +0700

    Membuat style di form-daftar.php

--- form-daftar.php
+++ form-daftar.php
@@ -1,39 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulir Pendaftaran Siswa Baru | SMK RPL</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Formulir Pendaftaran Siswa Baru</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header>
+    <form action="proses-pendaftaran.php" method="POST">
         <h3>Formulir Pendaftaran Siswa Baru</h3>
-    </header>
-    <form action="proses-pendaftaran.php" method="post">
-        <fieldset>
-            <label for="nama">Nama:</label>
-            <input type="text" name="nama" placeholder="Nama Lengkap" require><br><br>
-            <label for="alamat">Alamat:</label>
-            <textarea name="alamat" id="alamat" placeholder="Alamat Anda"></textarea><br><br>
-            <label for="jenisKelamin">Jenis Kelamin:</label>
-            <select name="jenisKelamin" id="jenisKelamin">
-                <option disabled selected></option>
-                <option value="Laki-laki">Laki - Laki</option>
-                <option value="Perempuan">Perempuan</option>
-            </select><br><br>
-            <label for="agama">Agama:</label>
-            <select name="agama" id="agama">
-                <option disabled selected></option>
-                <option>Islam</option> 
-                <option>Kristen</option> 
-                <option>Hindu</option=> 
-                <option>Buddha</option=> 
-            </select><br><br>
-            <label for="asalSekolah">Asal Sekolah:</label>
-            <input type="text" name="asalSekolah" id="asalSekolah" placeholder="Asal sekolah Anda" require><br><br>
-            <input type="submit" name="btnSubmit" id="btnSubmit" value="Kirim">
-        </fieldset>
+        
+        <label for="nama">Nama</label>
+        <input type="text" name="nama" id="nama" required>
+        
+        <label for="alamat">Alamat</label>
+        <input type="text" name="alamat" id="alamat" required>
+        
+        <label for="jenis_kelamin">Jenis Kelamin</label>
+        <select name="jenis_kelamin" id="jenis_kelamin" required>
+            <option value="Laki-laki">Laki-laki</option>
+            <option value="Perempuan">Perempuan</option>
+        </select>
+        
+        <label for="agama">Agama</label>
+        <select name="agama" id="agama" required>
+            <option value="Islam">Islam</option>
+            <option value="Kristen">Kristen</option>
+            <option value="Hindu">Hindu</option>
+            <option value="Budha">Budha</option>
+            <option value="Lainnya">Lainnya</option>
+        </select>
+        
+        <label for="sekolah_asal">Asal Sekolah</label>
+        <input type="text" name="sekolah_asal" id="sekolah_asal" required>
+        
+        <button type="submit">Submit</button>
     </form>
 </body>
 </html>
