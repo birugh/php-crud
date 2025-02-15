1ddd686d5877382a9a4d7d1cec9d9fe88906fd5d
birugh <notblue.https@gmail.com>
2025-02-15 23:02:47 +0700

    Mengganti id_siswa menjadi nisn_siswa

--- form-nilai.php
+++ form-nilai.php
@@ -1,7 +1,7 @@
 <?php
 include("config.php");
 
-$sql = "SELECT id_user, nama_siswa FROM tbl_siswa";
+$sql = "SELECT nisn_siswa, nama_siswa FROM tbl_siswa";
 $query = mysqli_query($db, $sql);
 ?>
 
@@ -15,25 +15,25 @@ $query = mysqli_query($db, $sql);
     <form action="proses-nilai.php" method="POST">
         <h3>Formulir Penilaian Siswa</h3>
         
-        <label for="nisn">NISN</label>
-        <select name="nisn" id="nisn" required>
+        <label for="nisn_siswa">NISN</label>
+        <select name="nisn_siswa" id="nisn_siswa" required>
             <option value="" disabled selected>Pilih NISN</option>
             <?php while ($siswa = mysqli_fetch_array($query)) { ?>
-                <option value="<?php echo $siswa['id_user']; ?>"><?php echo $siswa['id_user'] . " - " . $siswa['nama_siswa']; ?></option>
+                <option value="<?php echo $siswa['nisn_siswa']; ?>"><?php echo $siswa['nisn_siswa'] . " - " . $siswa['nama_siswa']; ?></option>
             <?php } ?>
         </select>
         
         <label for="nilai_mtk">Nilai Matematika</label>
-        <input type="number" name="nilai_mtk" id="nilai_mtk" required>
+        <input type="text" name="nilai_mtk" id="nilai_mtk" required>
         
         <label for="nilai_pjok">Nilai PJOK</label>
-        <input type="number" name="nilai_pjok" id="nilai_pjok" required>
+        <input type="text" name="nilai_pjok" id="nilai_pjok" required>
         
         <label for="nilai_pabp">Nilai PABP</label>
-        <input type="number" name="nilai_pabp" id="nilai_pabp" required>
+        <input type="text" name="nilai_pabp" id="nilai_pabp" required>
         
         <label for="nilai_web">Nilai Web</label>
-        <input type="number" name="nilai_web" id="nilai_web" required>
+        <input type="text" name="nilai_web" id="nilai_web" required>
         
         <button type="submit" name="btnSubmit">Submit</button>
     </form>
