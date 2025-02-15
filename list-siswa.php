93ea8a5d68050136915c59b3d83776739ddff517
birugh <notblue.https@gmail.com>
2025-02-15 23:58:29 +0700

    Mengatur ulang list siswa

--- list-siswa.php
+++ list-siswa.php
@@ -29,15 +29,15 @@ $query = mysqli_query($db, $sql);
             <tbody>
                 <?php while ($siswa = mysqli_fetch_array($query)) { ?>
                     <tr>
-                        <td><?php echo $siswa['id_user']; ?></td>
+                        <td><?php echo $siswa['nisn_siswa']; ?></td>
                         <td><?php echo $siswa['nama_siswa']; ?></td>
                         <td><?php echo $siswa['alamat']; ?></td>
                         <td><?php echo $siswa['jenis_kelamin']; ?></td>
                         <td><?php echo $siswa['agama']; ?></td>
                         <td><?php echo $siswa['asal_sekolah']; ?></td>
                         <td>
-                            <a href="form-edit.php?id_user=<?php echo $siswa['id_user']; ?>" class="btn-edit">Edit</a>
-                            <a href="hapus.php?id_user=<?php echo $siswa['id_user']; ?>" class="btn-hapus" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Hapus</a>
+                            <a href="form-edit-siswa.php?nisn_siswa=<?php echo $siswa['nisn_siswa']; ?>" class="btn-edit">Edit</a>
+                            <a href="hapus.php?nisn_siswa=<?php echo $siswa['nisn_siswa']; ?>" class="btn-hapus" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Hapus</a>
                         </td>
                     </tr>
                 <?php } ?>
